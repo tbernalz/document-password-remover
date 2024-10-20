61599b8d4106fc0782ceeb51785a739920257888
Tomás Bernal Zuluaga <tbernalz@eafit.edu.co>
2024-10-20 16:54:45 -0500

    :building_construction: Add scalable structure for cli and gui modes to handle more document types

--- src/cli.py
+++ src/cli.py
@@ -1,31 +1,56 @@
 import argparse
 import getpass
 import logging
-from src.pdf_handler import remove_pdf_password
+
+from src.file_handler import remove_password
+from src.enum.document_types import DocumentType
 
 
 def main(remaining_args):
     parser = argparse.ArgumentParser(
         description="CLI mode: Remove password from documents"
     )
-    parser.add_argument("--input", help="Path to the input PDF file")
+    parser.add_argument(
+        "--document",
+        help=f"Document type to remove password from. Supported types: {', '.join([doc.value for doc in DocumentType])}",
+    )
+    parser.add_argument("--input", help="Path to the input file")
     parser.add_argument("--output", help="Path to save the output decrypted file")
     parser.add_argument("--password", help="Password to unlock the file")
 
     args = parser.parse_args(remaining_args)
 
-    input_pdf = args.input or input("Please enter the path to the input PDF file: ")
-    output_pdf = args.output or input("Please enter the path to save the output file: ")
-    password = args.password or getpass.getpass("Please enter the PDF password: ")
+    available_docs = ", ".join([doc.value for doc in DocumentType])
+    document = (
+        args.document
+        or input(f"Please enter the document type ({available_docs}): ").lower()
+    )
 
-    if not input_pdf or not output_pdf or not password:
+    try:
+        document_type = DocumentType(document)
+    except ValueError:
+        logging.error(
+            f"Unsupported document type: {document}. Supported types: {available_docs}"
+        )
+        return
+    input_file = args.input or input(
+        f"Please enter the path to the input {document} file: "
+    )
+    output_file = args.output or input(
+        "Please enter the path to save the output file: "
+    )
+    password = args.password or getpass.getpass(
+        f"Please enter the {document} password: "
+    )
+
+    if not input_file or not output_file or not password:
         logging.error("Missing required arguments: input, output, or password.")
         parser.print_help()
         return
 
     try:
-        remove_pdf_password(input_pdf, output_pdf, password)
-        logging.info(f"Password removed from {input_pdf} and saved to {output_pdf}")
+        remove_password(input_file, output_file, password)
+        logging.info(f"Password removed from {input_file} and saved to {output_file}")
     except Exception as e:
         logging.error(f"Failed to remove password: {e}", exc_info=True)
         raise RuntimeError(f"Failed to remove password: {e}")
--- src/gui.py
+++ src/gui.py
@@ -2,7 +2,8 @@ import tkinter as tk
 import logging
 from tkinter import filedialog, messagebox
 
-from src.pdf_handler import remove_pdf_password
+from src.file_handler import remove_password
+from src.enum.document_types import DocumentType
 
 
 def main():
@@ -10,25 +11,33 @@ def main():
     root.title("Document Password Remover")
 
     def select_input_file():
-        input_file = filedialog.askopenfilename(title="Select PDF File")
-        input_pdf_entry.delete(0, tk.END)
-        input_pdf_entry.insert(0, input_file)
-        logging.info(f"Input PDF selected: {input_file}")
+        selected_type = document_type_var.get().lower()
+        file_types = [(f"{selected_type.upper()} Files", f"*.{selected_type}")]
+        input_file = filedialog.askopenfilename(
+            title=f"Select {selected_type.upper()} File", filetypes=file_types
+        )
+        input_file_entry.delete(0, tk.END)
+        input_file_entry.insert(0, input_file)
+        logging.info(f"Input {selected_type.upper()} selected: {input_file}")
 
     def select_output_file():
+        selected_type = document_type_var.get().lower()
         output_file = filedialog.asksaveasfilename(
-            title="Save Decrypted PDF As", defaultextension=".pdf"
+            title=f"Save Decrypted {selected_type.upper()} As",
+            defaultextension=f".{selected_type}",
+            filetypes=[(f"{selected_type.upper()} Files", f"*.{selected_type}")],
         )
-        output_pdf_entry.delete(0, tk.END)
-        output_pdf_entry.insert(0, output_file)
-        logging.info(f"Output PDF selected: {output_file}")
+        output_file_entry.delete(0, tk.END)
+        output_file_entry.insert(0, output_file)
+        logging.info(f"Output {selected_type.upper()} selected: {output_file}")
 
-    def decrypt_pdf():
-        input_pdf = input_pdf_entry.get()
-        output_pdf = output_pdf_entry.get()
+    def decrypt_document():
+        input_file = input_file_entry.get()
+        output_file = output_file_entry.get()
         password = password_entry.get()
+        document_type = document_type_var.get().lower()
 
-        if not input_pdf or not output_pdf or not password:
+        if not input_file or not output_file or not password:
             messagebox.showerror(
                 "Error",
                 "All fields (Input Path, Output Path, and Password) must be provided.",
@@ -37,45 +46,58 @@ def main():
             return
 
         try:
-            remove_pdf_password(input_pdf, output_pdf, password)
+            remove_password(input_file, output_file, password)
             logging.info(
-                f"Success: Password removed from {input_pdf} and saved as {output_pdf}"
+                f"Success: Password removed from {input_file} and saved as {output_file}"
             )
             messagebox.showinfo(
                 "Success",
-                f"Password removed from {input_pdf} and saved as {output_pdf}.",
+                f"Password removed from {input_file} and saved as {output_file}.",
             )
         except Exception as e:
             logging.error(
-                f"Error: Failed to remove password from {input_pdf}: {e}", exc_info=True
+                f"Error: Failed to remove password from {input_file}: {e}",
+                exc_info=True,
             )
             messagebox.showerror("Error", f"Failed to remove password: {e}")
 
-    input_pdf_label = tk.Label(root, text="Input PDF:")
-    input_pdf_label.grid(row=0, column=0, padx=10, pady=5)
+    document_type_label = tk.Label(root, text="Document Type:")
+    document_type_label.grid(row=0, column=0, padx=10, pady=5)
 
-    input_pdf_entry = tk.Entry(root, width=40)
-    input_pdf_entry.grid(row=0, column=1, padx=10, pady=5)
+    document_type_var = tk.StringVar(root)
+    document_type_var.set(DocumentType.PDF.value)
 
-    input_pdf_button = tk.Button(root, text="Browse...", command=select_input_file)
-    input_pdf_button.grid(row=0, column=2, padx=10, pady=5)
+    document_type_dropdown = tk.OptionMenu(
+        root, document_type_var, *[doc.value.upper() for doc in DocumentType]
+    )
+    document_type_dropdown.grid(row=0, column=1, padx=10, pady=5)
 
-    output_pdf_label = tk.Label(root, text="Output PDF:")
-    output_pdf_label.grid(row=1, column=0, padx=10, pady=5)
+    input_file_label = tk.Label(root, text="Input File:")
+    input_file_label.grid(row=1, column=0, padx=10, pady=5)
 
-    output_pdf_entry = tk.Entry(root, width=40)
-    output_pdf_entry.grid(row=1, column=1, padx=10, pady=5)
+    input_file_entry = tk.Entry(root, width=40)
+    input_file_entry.grid(row=1, column=1, padx=10, pady=5)
 
-    output_pdf_button = tk.Button(root, text="Browse...", command=select_output_file)
-    output_pdf_button.grid(row=1, column=2, padx=10, pady=5)
+    input_file_button = tk.Button(root, text="Browse...", command=select_input_file)
+
+    input_file_button.grid(row=1, column=2, padx=10, pady=5)
+
+    output_file_label = tk.Label(root, text="Output File:")
+    output_file_label.grid(row=2, column=0, padx=10, pady=5)
+
+    output_file_entry = tk.Entry(root, width=40)
+    output_file_entry.grid(row=2, column=1, padx=10, pady=5)
+
+    output_file_button = tk.Button(root, text="Browse...", command=select_output_file)
+    output_file_button.grid(row=2, column=2, padx=10, pady=5)
 
     password_label = tk.Label(root, text="Password:")
-    password_label.grid(row=2, column=0, padx=10, pady=5)
+    password_label.grid(row=3, column=0, padx=10, pady=5)
 
     password_entry = tk.Entry(root, show="*", width=40)
-    password_entry.grid(row=2, column=1, padx=10, pady=5)
+    password_entry.grid(row=3, column=1, padx=10, pady=5)
 
-    decrypt_button = tk.Button(root, text="Remove Password", command=decrypt_pdf)
-    decrypt_button.grid(row=3, columnspan=3, pady=10)
+    decrypt_button = tk.Button(root, text="Remove Password", command=decrypt_document)
+    decrypt_button.grid(row=4, columnspan=3, pady=10)
 
     root.mainloop()
